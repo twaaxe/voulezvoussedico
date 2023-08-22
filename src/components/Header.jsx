4cade412e480b318a81918b1120820d214fec48b
twaaxe <35602018+twaaxe@users.noreply.github.com>
2023-08-22 15:34:56 +0200

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import '../style/Header.css'
 
@@ -19,17 +18,6 @@ export default function Header() {
                         <Nav className="me-auto">
                             <Nav.Link href="home">Pro</Nav.Link>
                             <Nav.Link href="home">Particulier</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
                         </Nav>
                         <Nav>
                             <Nav.Link href="aboutus">About us</Nav.Link>
